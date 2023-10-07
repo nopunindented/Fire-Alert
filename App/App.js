61c341a96de640d674792caf5bf76690a8fd989a
David Urschel <durschel@ualberta.ca>
2023-10-07 13:44:44 -0600

    Primary test

--- App/App.js
+++ App/App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { fetchNASAData } from './util/firmsApi';
 
 export default function App() {
+    useEffect(() => {
+        // Call the function to fetch NASA data when the component mounts
+        fetchNASAData()
+          .then(data => {
+            // Handle the data as needed (e.g., update state or display it)
+            console.log('Response data:', data);
+            setDa(data);
+          })
+          .catch(error => {
+            // Handle errors here (e.g., show an error message)
+            console.error('Error:', error);
+            setDa("failed")
+          });
+      }, []);
+
+      const (da, setDa) = useState("null" as any);
+      const da2 = "boom"
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This app uses FIRMS! ??A {da2}</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ App/util/firmsApi.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const FIRMS_MAP_KEY = "832b6eef2f6837122b1999958dddac1e";
+
+export const fetchNASAData = async () => {
+  const url = `https://firms.modaps.eosdis.nasa.gov/api/area/csv/${FIRMS_MAP_KEY}/VIIRS_SNPP_NRT/world/1/2023-10-07`;
+
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
+  }
+};
