8cc6c1fa6830c194d12e4df2f9194f8efefa3a40
Matthew Thomson <mdthomso@ualberta.ca>
2023-10-07 18:28:50 -0600

    query now happens with current date

--- App/util/firmsApi.js
+++ App/util/firmsApi.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 const FIRMS_MAP_KEY = "832b6eef2f6837122b1999958dddac1e";
+const current = new Date();
+const date = `${current.getFullYear()}-${current.getMonth()+1}-${current.getDate()}`
 
 export const fetchNASAData = async () => {
-  const url = `https://firms.modaps.eosdis.nasa.gov/api/area/csv/${FIRMS_MAP_KEY}/VIIRS_SNPP_NRT/world/1/2023-10-07`;
+  const url = `https://firms.modaps.eosdis.nasa.gov/api/area/csv/${FIRMS_MAP_KEY}/VIIRS_SNPP_NRT/world/1/${date}`;
 
   try {
     const response = await axios.get(url);
