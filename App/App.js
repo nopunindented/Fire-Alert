74805167b45647e2362525274d8cf4f7318c8409
David Urschel <durschel@ualberta.ca>
2023-10-08 11:09:10 -0600

    Stuff

--- App/App.js
+++ App/App.js
@@ -5,30 +5,50 @@ import { fetchNASAData } from './util/firmsApi';
 import Map from './Map';
 
 export default function App() {
+        const [loadingFirmsData, setLoadingFirmsData] = useState(true);
+        const [headers, setHeaders] = useState([]);
+        const [firmsData, setFirmsData] = useState([]);
+        const [lati, setLati] = useState(10.5461);
+        const [lngi, setLngi] = useState(10.4937);
+
+    const [userLocation, setUserLocation] = useState({ latitude: 0, longitude: 0 });
+
+      useEffect(() => {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(
+            position => {
+              setLati(position.coords.latitude);
+              setLongi(position.coords.longitude);
+            },
+            error => console.error(error),
+            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+          );
+        } else {
+          console.error("Geolocation is not supported by this device.");
+        }
+      }, []);
+
     useEffect(() => {
+        console.log("Started Loading Data")
         setLoadingFirmsData(true)
-        fetchNASAData()
-          .then(data => {
+        fetchNASAData(lat=lati, lng=lngi).then(data => {
             setLoadingFirmsData(false);
             setHeaders(data[0]);
             setFirmsData(data[1]);
             console.log("Finished Loading Data")
-            console.log(data[1].length)
+            console.log(data[1].length + " points loaded")
 })
           .catch(error => {
             console.error('Error:', error);
           });
       }, []);
 
-        const [loadingFirmsData, setLoadingFirmsData] = useState(true);
-        const [headers, setHeaders] = useState([]);
-        const [firmsData, setFirmsData] = useState([]);
 
   return (
     <View style={styles.container}>
       <Text>This app uses FIRMS! {loadingFirmsData ? ("data is loading...") : ("data is loaded")}</Text>
       <StatusBar style="auto" />
-      {!loadingFirmsData && <Map lat={1} lng={1} fireData={firmsData} />}
+      {!loadingFirmsData && firmsData && <Map lat={lati} lng={lngi} fireData={firmsData} />}
     </View>
   );
 }
