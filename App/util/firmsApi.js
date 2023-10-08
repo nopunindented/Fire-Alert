cadcf5c7e2eea7dcfa9592fb9fcc32e1f0f3ea71
David Urschel <durschel@ualberta.ca>
2023-10-08 11:47:51 -0600

    Works except geolocation

--- App/util/firmsApi.js
+++ App/util/firmsApi.js
@@ -3,10 +3,9 @@ import axios from 'axios';
 const FIRMS_MAP_KEY = "832b6eef2f6837122b1999958dddac1e";
 
 export const fetchNASAData = async (lat, lng) => {
-  const url = `https://firms.modaps.eosdis.nasa.gov/api/area/csv/${FIRMS_MAP_KEY}/VIIRS_SNPP_NRT/${lat-10},${lng-10},${lat+10},${lng+10}/1/2023-10-07`;
-
-  try {
+  const url = `https://firms.modaps.eosdis.nasa.gov/api/area/csv/${FIRMS_MAP_KEY}/VIIRS_SNPP_NRT/${lng-10},${lat-10},${lng+10},${lat+10}/1/2023-10-07`;
     console.log(url)
+  try {
     const response = await axios.get(url);
     const dataArray = [];
         const lines = response.data.trim().split('\n');
