2ad19c2b29bb77da3fbbfdf229cf575a91fde30a
umerindented <umerfiaz251@gmail.com>
2023-10-08 21:19:07 -0600

    "Cluster app"

--- Scisat/Cluster.py
+++ Scisat/Cluster.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as ps
 from io import StringIO
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 app = Flask(__name__)
 
 @app.route('/api/cluster', methods=['GET'])
@@ -19,23 +19,31 @@ def cluster():
 
     url = f"https://firms.modaps.eosdis.nasa.gov/api/area/csv/{FIRMS_MAP_KEY}/VIIRS_SNPP_NRT/world/1/{date}"
 
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        # csv_string = str(response.content)
-        # data = ps.DataFrame([x.split(';') for x in csv_string.split('\n')])
-        # data = ps.read_csv(,dtype=str)
-        line_data = np.char.split(np.array((response.content.decode()).strip().split('\n')),sep=',')
-        data = np.array([np.array(line) for line in line_data])
-        df = ps.DataFrame(data=data[1:,:],columns=data[0,:])
-        print(data)
-        print(data.shape)
-        print(df)
-        print(df.shape)
-    else:
-        # Handle errors, e.g., print the status code and error message
+    response = None
+    try:
+        response = requests.get(url)
+    except:
         print(f"API request failed with status code {response.status_code}: {response.text}")
+        abort(502,f"API request failed with status code {response.status_code}: {response.text}")
         
+    
+    if (response is not None):
+        if (response.status_code == 200):
+            # csv_string = str(response.content)
+            # data = ps.DataFrame([x.split(';') for x in csv_string.split('\n')])
+            # data = ps.read_csv(,dtype=str)
+            line_data = np.char.split(np.array((response.content.decode()).strip().split('\n')),sep=',')
+            data = np.array([np.array(line) for line in line_data])
+            df = ps.DataFrame(data=data[1:,:],columns=data[0,:])
+            print(data)
+            print(data.shape)
+            print(df)
+            print(df.shape)
+        else:
+            # Handle errors, e.g., print the status code and error message
+            print(f"API request failed with status code {response.status_code}: {response.text}")
+            abort(500,f"API request failed with status code {response.status_code}: {response.text}")
+                
     coordinates = df[["latitude","longitude"]].to_numpy().astype(float)
     filter = np.array([confidence > 30 if isinstance(confidence,float) else ((confidence == "n") | (confidence == "h")) for confidence in df["confidence"]])
     print(coordinates[filter])
@@ -45,45 +53,26 @@ def cluster():
     unique_labels = set(labels)
     core_samples_mask = np.zeros_like(labels, dtype=bool)
     core_samples_mask[db.core_sample_indices_] = True
-    colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
-    for k, col in zip(unique_labels, colors):
-        if k == -1:
-            # Black used for noise.
-            col = [0, 0, 0, 1]
-
-        class_member_mask = labels == k
-
-        colour = np.random.rand(3,)
-
-        xy = coordinates[filter][class_member_mask]
-        plt.plot(
-            xy[:, 1],
-            xy[:, 0],
-            "o",
-            markerfacecolor=colour,
-            markeredgecolor="k",
-            markersize=14,
-        )
-
-        # xy = coordinates[class_member_mask & ~core_samples_mask]
-        # plt.plot(
-        #     xy[:, 1],
-        #     xy[:, 0],
-        #     "o",
-        #     markerfacecolor=tuple(col),
-        #     markeredgecolor="k",
-        #     markersize=6,
-        # )
-
     print(labels.size)
     print(len(unique_labels))
+    # colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
+    grouped_fires = np.zeros((len(unique_labels)+np.sum(labels==-1)-1,3))
+    for k in unique_labels:
+        if k == -1:
+            pass
+        average_coordinate = np.mean(coordinates[filter][labels==k],axis=0)
+        grouped_fires[k] = np.array([average_coordinate[0],average_coordinate[1],k])
 
-    plt.title(f"Estimated number of clusters:")
-    plt.gca().set_aspect(1.2)
+    unique_coordinates = coordinates[filter][labels==-1]
+    grouped_fires[len(unique_labels)-1:] = np.c_[unique_coordinates,-np.ones(len(unique_coordinates))] # Chat GPT improved after lots of coercing
+  
+
+    # plt.title(f"Estimated number of clusters:")
+    # plt.gca().set_aspect(1.2)
     # plt.show()
 
     # Return the result as a JSON response
-    return jsonify(db.labels_.tolist())
+    return jsonify(grouped_fires.tolist())
 
 
 
