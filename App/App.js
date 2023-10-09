02e2b57aead2c13c495f5d112b4927f2224c6655
umerindented <umerfiaz251@gmail.com>
2023-10-08 22:47:35 -0600

    "changes"

--- App/App.js
+++ App/App.js
@@ -1,76 +1,91 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { fetchNASAData } from './util/firmsApi';
 import Map from './Map';
 import { PermissionsAndroid } from 'react-native';
 
-const apiUrl = 'http://192.168.56.2:5000/api/cluster';
-
 export default function App() {
-  const [loadingFirmsData, setLoadingFirmsData] = useState(true);
-  const [firmsData, setFirmsData] = useState([]);
-  const [lati, setLati] = useState(53.5461);
-  const [lngi, setLngi] = useState(-113.4937);
+        const [loadingFirmsData, setLoadingFirmsData] = useState(true);
+        const [headers, setHeaders] = useState([]);
+        const [firmsData, setFirmsData] = useState([]);
+        const [lati, setLati] = useState(53.5461);
+        const [lngi, setLngi] = useState(-113.4937);
 
-  async function requestLocationPermission() {
-    try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-        {
-          title: 'Location Permission',
-          message: 'This app needs access to your location for geolocation features.',
-          buttonNeutral: 'Ask Me Later',
-          buttonNegative: 'Cancel',
-          buttonPositive: 'OK',
-        }
-      );
+    const [userLocation, setUserLocation] = useState({ latitude: 0, longitude: 0 });
 
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        console.log('Location permission granted');
-        // You can now call navigator.geolocation.getCurrentPosition
-      } else {
-        console.log('Location permission denied');
+async function requestLocationPermission() {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: 'Location Permission',
+        message: 'This app needs access to your location for geolocation features.',
+        buttonNeutral: 'Ask Me Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
       }
-    } catch (err) {
-      console.warn(err);
-    }
-  }
+    );
 
-  useEffect(() => {
-    console.log('Started Loading Data');
-    setLoadingFirmsData(true);
-    requestLocationPermission().then(() => {
-      fetch(apiUrl)
-        .then((response) => response.json())
-        .then((data) => {
-          setLoadingFirmsData(false);
-          setFirmsData(data);
-          console.log('Finished Loading Data');
-          console.log(data.length + ' points loaded');
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      console.log('Location permission granted');
+      // You can now call navigator.geolocation.getCurrentPosition
+    } else {
+      console.log('Location permission denied');
+    }
+  } catch (err) {
+    console.warn(err);
+  }
+}
+
+
+     async function getLocation() {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(
+            position => {
+              setLati(position.coords.latitude);
+              setLngi(position.coords.longitude);
+            },
+            error => console.error(error),
+            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+          );
+        } else {
+          console.error("Geolocation is not supported by this device.");
+        }
+     };
+
+    useEffect(() => {
+        console.log("Started Loading Data")
+        setLoadingFirmsData(true)
+        requestLocationPermission().then(
+        fetchNASAData(lat=lati, lng=lngi).then(data => {
+            setLoadingFirmsData(false);
+            setHeaders(data[0]);
+            setFirmsData(data[1]);
+            console.log("Finished Loading Data")
+            console.log(data[1].length + " points loaded")
         })
-        .catch((error) => {
-          console.error('Error:', error);
-        });
-    });
-  }, []);
+          .catch(error => {
+            console.error('Error:', error);
+          }));
+      }, []);
+
 
   return (
     <View style={styles.container}>
-      <Text>{loadingFirmsData ? 'Data is loading...' : 'Data is loaded'}</Text>
-      <StatusBar style='auto' />
-      <View
-        style={{
-          width: '100%',
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-around',
-          backgroundColor: 'darkblue',
-          padding: 10,
-        }}
-      >
+      <Text>{loadingFirmsData ? ("data is loading...") : ("data is loaded")}</Text>
+      <StatusBar style="auto" />
+      <View style={{ width: '100%', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around', backgroundColor: 'darkblue', padding: 10 }}>
         <Text style={{ fontWeight: 'bold', color: 'white' }}>This App Uses FIRMS by </Text>
-        <Image style={{ width: 120, height: 100 }} source={require('./assets/NASA.png')} />
+        <Image
+          style={{ width: 120, height: 100 }}
+          source={require('./assets/NASA.png')}
+        />
       </View>
+
+
+
+
       {!loadingFirmsData && firmsData && <Map lat={lati} lng={lngi} fireData={firmsData} />}
     </View>
   );
@@ -82,5 +97,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
